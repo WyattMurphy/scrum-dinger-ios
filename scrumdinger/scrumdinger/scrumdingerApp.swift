0ef069bfbcbeb5d65a5ae68532a83ab1c57254ca
WyattMurphy <wmurph456@gmail.com>
2024-02-20 12:48:15 -0500

    First Commit

---
+++ scrumdinger/scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Wyatt Murphy on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
